49376fef511543554f0082368acce28b520379fd
Kai Gibson <kai@Admins-MacBook-Pro.local>
2025-09-03 18:26:45 +1000

    add tests for all sample data
    Make xml models work irrespective of field order

--- app/models/import_marks_request.py
+++ app/models/import_marks_request.py
@@ -1,22 +1,28 @@
 from pydantic_xml import BaseXmlModel, attr, element
+from pydantic import ConfigDict
 from datetime import datetime
 from typing import List, Optional
 
 
-class SummaryMarks(BaseXmlModel, tag="summary-marks"):
+class SummaryMarks(BaseXmlModel, tag="summary-marks", search_mode="unordered"):
+    model_config = ConfigDict(extra="ignore")
     available: int = attr(name="available")
     obtained: int = attr(name="obtained")
 
-
 # names are marked optional in case they don't get scanned properly by the marking machine
-class MCQTestResult(BaseXmlModel, tag="mcq-test-result"):
+class MCQTestResult(BaseXmlModel, tag="mcq-test-result", search_mode="unordered"):
+    model_config = ConfigDict(extra="ignore")
     scanned_on: datetime = attr(name="scanned-on")
     first_name: Optional[str] = element(tag="first-name")
     last_name: Optional[str] = element(tag="last-name")
     student_number: str = element(tag="student-number")
     test_id: int = element(tag="test-id")
-    summary_marks: SummaryMarks = element()
+    # answer: Optional[str] = element(tag="answer")
+    summary_marks: Optional[SummaryMarks] = element(tag='summary-marks')
 
 
-class ImportMarksRequest(BaseXmlModel, tag="mcq-test-results"):
+
+
+class ImportMarksRequest(BaseXmlModel, tag="mcq-test-results", search_mode="unordered"):
+    model_config = ConfigDict(extra="ignore")
     mcq_test_results: List[MCQTestResult] = element(tag="mcq-test-result")
--- tests/test_model.py
+++ tests/test_model.py
@@ -14,7 +14,6 @@ xml_data = """
 </mcq-test-results>
 """
 
-
 def test_import_marks_request():
     request = ImportMarksRequest.from_xml(xml_data)
 
@@ -23,6 +22,60 @@ def test_import_marks_request():
     result = request.mcq_test_results[0]
     assert result.first_name == "Jane"
     assert result.last_name == "Austen"
+    assert result.student_number == "521585128"
     assert result.test_id == 1234
     assert result.summary_marks.available == 20
     assert result.summary_marks.obtained == 13
+
+xml_data_from_sample = """
+<mcq-test-results>
+	<mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">
+		<first-name>KJ</first-name>
+		<last-name>Alysander</last-name>
+		<student-number>002299</student-number>
+		<test-id>9863</test-id>
+		<answer question="0" marks-available="1" marks-awarded="1">D</answer>
+		<answer question="1" marks-available="1" marks-awarded="1">D</answer>
+		<answer question="2" marks-available="1" marks-awarded="1">D</answer>
+		<answer question="3" marks-available="1" marks-awarded="0">C</answer>
+		<answer question="4" marks-available="1" marks-awarded="1">B</answer>
+		<answer question="5" marks-available="1" marks-awarded="0">D</answer>
+		<answer question="6" marks-available="1" marks-awarded="0">A</answer>
+		<answer question="7" marks-available="1" marks-awarded="1">A</answer>
+		<answer question="8" marks-available="1" marks-awarded="1">B</answer>
+		<answer question="9" marks-available="1" marks-awarded="1">D</answer>
+		<answer question="10" marks-available="1" marks-awarded="1">A</answer>
+		<answer question="11" marks-available="1" marks-awarded="1">B</answer>
+		<answer question="12" marks-available="1" marks-awarded="0">A</answer>
+		<answer question="13" marks-available="1" marks-awarded="0">B</answer>
+		<answer question="14" marks-available="1" marks-awarded="1">B</answer>
+		<answer question="15" marks-available="1" marks-awarded="1">A</answer>
+		<answer question="16" marks-available="1" marks-awarded="1">C</answer>
+		<answer question="17" marks-available="1" marks-awarded="0">B</answer>
+		<answer question="18" marks-available="1" marks-awarded="1">A</answer>
+		<answer question="19" marks-available="1" marks-awarded="0">B</answer>
+		<summary-marks available="20" obtained="13" />
+	</mcq-test-result>
+</mcq-test-results>
+"""
+
+def test_import_marks_single_from_sample():
+    request = ImportMarksRequest.from_xml(xml_data_from_sample)
+
+    assert len(request.mcq_test_results) == 1
+
+    result = request.mcq_test_results[0]
+    assert result.first_name == "KJ"
+    assert result.last_name == "Alysander"
+    assert result.student_number == "002299"
+    assert result.test_id == 9863
+    assert result.summary_marks.available == 20
+    assert result.summary_marks.obtained == 13
+    
+def test_import_marks_sample_results():
+    sample_data: str
+    with open('sample_data/sample_results.xml', 'r') as sample_results:
+        sample_data = sample_results.read()
+    
+    request = ImportMarksRequest.from_xml(sample_data)
+    assert len(request.mcq_test_results) == 100
