6c43bc2245f8546896572327f09e8b908460b5f7
Kai Gibson <kai@Admins-MacBook-Pro.local>
2025-09-03 17:00:08 +1000

    add aggregate json schema

---
+++ app/models/aggregate_response.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+"""
+{
+  "mean": float,
+  "stddev": float,
+  "min": float,
+  "max": float,
+  "p25": float,
+  "p50": float,
+  "p75": float,
+  "count": int
+}
+"""
+class AggregateResponse(BaseModel):
+    mean: float
+    stddev: float
+    min: float
+    max: float
+    p25: float
+    p50: float
+    p75: float
+    count: int
+
