c91b089af1c40b65d1bc85296dfbb6b14e7f0b12
Kai Gibson <kai@Admins-MacBook-Pro.local>
2025-09-03 16:15:50 +1000

    create import marks XML schema

--- app/api/import_marks.py
+++ app/api/import_marks.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request, HTTPException
 from sqlmodel import Session
 from app.database import get_session
 from app.models.results import Results
+from app.models.import_marks_request import ImportMarksRequest
 
 router = APIRouter()
 
 @router.post("/import")
-def import_marks(session: Session = Depends(get_session)):
+async def import_marks(request: Request, session: Session = Depends(get_session)):
+    body_bytes = await request.body()
+    content_type = request.headers.get("content-type", "")
+    if "text/xml+markr" not in content_type:
+        raise HTTPException(status_code=415, detail="Incorrect media type") 
+
+    body = ImportMarksRequest.from_xml(body_bytes)
+    print(body)
     return {"test":"message"}
---
+++ app/models/import_marks_request.py
@@ -0,0 +1,19 @@
+from pydantic_xml import BaseXmlModel, attr, element
+from pydantic import Field
+from datetime import datetime
+from typing import List, Optional
+
+class SummaryMarks(BaseXmlModel, tag="summary-marks"):
+    available: int = attr(name="available")
+    obtained: int = attr(name="obtained")
+
+class MCQTestResult(BaseXmlModel, tag="mcq-test-result"):
+    scanned_on: datetime = attr(name="scanned-on") 
+    first_name: Optional[str] = element(tag="first-name")
+    last_name: Optional[str] = element(tag="last-name")
+    student_number: str = element(tag="student-number")
+    test_id: int = element(tag="test-id") 
+    summary_marks: SummaryMarks = element()
+
+class ImportMarksRequest(BaseXmlModel, tag="mcq-test-results"):
+    mcq_test_results: List[MCQTestResult] = element(tag="mcq-test-result")
